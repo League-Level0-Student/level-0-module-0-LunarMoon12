6eb3cdb850fccb0693911f00f0b47e71380c9f80
root <root@6b8dedb332b6>
2022-08-03 23:52:32 +0000

    Maze, Awesome or not, Quiz game

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,55 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
-		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+
+		// Create a variable to hold the user's score. Set it equal to zero.
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
-		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
-		// After all the questions have been asked, tell the user their final score 
-		
+
+		// 2. Ask the user a question
+		String question = JOptionPane.showInputDialog("Can you draw a cube?");
+
+		// 3. Use an if statement to check if their answer is correct
+		if (question.equals("Yes")) {
+			// 4. if the user's answer was correct, add one to their score
+			JOptionPane.showMessageDialog(null, "correct");
+			score++;
+		} else {
+			score--;
+			JOptionPane.showMessageDialog(null, "Yes");
+
+		}
+		// MAKE MORE QUESTIONS. Ask more questions by repeating the above
+		// Option: Subtract a point from their score for a wrong answer
+		String next_question = JOptionPane.showInputDialog("Can you write the word pentagon?");
+		if (next_question.equals("Pentagon")) {
+			JOptionPane.showMessageDialog(null, "correct");
+			score++;
+		} else {
+			score--;
+			JOptionPane.showMessageDialog(null, "Pentagon");
+		}
+		String next2question = JOptionPane.showInputDialog("What does tri mean?");
+		if (next2question.equals("3")) {
+			JOptionPane.showMessageDialog(null, "correct");
+			score++;
+		} else {
+			score--;
+
+			JOptionPane.showMessageDialog(null, "3");
+		}
+		String next3question = JOptionPane.showInputDialog("How many sides does a heptagon have?");
+		if (next3question.equals("7")) {
+			JOptionPane.showMessageDialog(null, "correct");
+			score++;
+		} else {
+			score--;
+			JOptionPane.showMessageDialog(null, "7");
+		}
+		// After all the questions have been asked, tell the user their final score
+		JOptionPane.showMessageDialog(null, "Your score is" + score);
 	}
 }
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,35 +2,41 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
+public static void main(String[] args) {
+	
 
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
 		// 2. Make a variable that will hold a random whole number
-	
+	int random = ran.nextInt(4);
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
+	System.out.println(random);
 		// 3. Print your variable to the console
-	
+	JOptionPane.showInputDialog("What is something awesome?");
 		// 4. Get the user to enter something that they think is awesome
-	
+	if (random == 0){
 		// 5. If your variable is  0
-	
+	JOptionPane.showMessageDialog(null, "That's awesome!");
 			// -- tell the user whatever they entered is awesome!
-	
+	}
 		// 6. If your variable is  1
-	
+	else if (random == 1) {
 			// -- tell the user whatever they entered is ok.
-	
+	JOptionPane.showMessageDialog(null,"That's ok.");
+	}	
 		// 7. If your variable is  2
-	
+	else if (random == 2) {
 			// -- tell the user whatever they entered is boring.
-	
+	JOptionPane.showMessageDialog(null, "That's boring.");}
 		// 8. If your variable is  3
-	
+	else if (random == 3) {
 			// -- invent your own message to give to the user (be nice).
-
+  JOptionPane.showMessageDialog(null, "That's neat!");}
+}
 }
